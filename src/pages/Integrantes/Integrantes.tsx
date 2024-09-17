f918d6a837e586b28aeffceb413ded3c1c3ea183
Alexsandro Macedo <97038931+AlexxsandroM@users.noreply.github.com>
2024-09-16 21:12:58 -0300

    Nomes e RMs de integrantes

--- src/pages/Integrantes/Integrantes.tsx
+++ src/pages/Integrantes/Integrantes.tsx
@@ -5,19 +5,19 @@ const Integrantes = () => {
       <div className="grid grid-cols-1 md:grid-cols-3 gap-12">
         {/* Card 1 */}
         <div className="bg-[#0046BF] text-white rounded-lg shadow-lg p-12 w-full h-full flex flex-col items-center justify-center min-h-[300px]">
-        <p className="text-xl mt-4 font-bold">Nome: </p>
-          <h2 className="text-3xl font-bold">RM: </h2>
+        <p className="text-xl mt-4 font-bold">Nome: Alexsandro de Macedo de Jesus </p>
+          <h2 className="text-3xl font-bold">RM: 557068 </h2>
           
         </div>
         {/* Card 2 */}
         <div className="bg-[#0046BF] text-white rounded-lg shadow-lg p-12 w-full h-full flex flex-col items-center justify-center min-h-[300px]">
-          <p className="text-xl mt-4 font-bold">Nome: </p>
-          <h2 className="text-3xl font-bold">RM: </h2>
+          <p className="text-xl mt-4 font-bold">Nome:Leonardo Faria Salazar </p>
+          <h2 className="text-3xl font-bold">RM: 557484 </h2>
         </div>
         {/* Card 3 */}
         <div className="bg-[#0046BF] text-white rounded-lg shadow-lg p-12 w-full h-full flex flex-col items-center justify-center min-h-[300px]">
-          <p className="text-xl mt-4 font-bold">Nome: </p>
-          <h2 className="text-3xl font-bold">RM: </h2>
+          <p className="text-xl mt-4 font-bold">Nome: Jonathan Matheus Rasne Pereira Fonseca </p>
+          <h2 className="text-3xl font-bold">RM: 558437 </h2>
         </div>
       </div>
     </div>
