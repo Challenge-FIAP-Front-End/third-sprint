00dc0dc835c5dbe97b29fcf5bed3f39592eb2f27
Alexsandro Macedo <97038931+AlexxsandroM@users.noreply.github.com>
2024-09-16 21:20:55 -0300

    Att Integrantes

--- src/pages/Integrantes/Integrantes.tsx
+++ src/pages/Integrantes/Integrantes.tsx
@@ -7,7 +7,6 @@ const Integrantes = () => {
         <div className="bg-[#0046BF] text-white rounded-lg shadow-lg p-12 w-full h-full flex flex-col items-center justify-center min-h-[300px]">
         <p className="text-xl mt-4 font-bold">Nome: Alexsandro de Macedo de Jesus </p>
           <h2 className="text-3xl font-bold">RM: 557068 </h2>
-          
         </div>
         {/* Card 2 */}
         <div className="bg-[#0046BF] text-white rounded-lg shadow-lg p-12 w-full h-full flex flex-col items-center justify-center min-h-[300px]">
